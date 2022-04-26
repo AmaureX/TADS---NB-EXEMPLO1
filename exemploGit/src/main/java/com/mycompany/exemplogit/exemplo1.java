db5e57b730c11de88f21e9c76f46e0a954df611b
AmaureX <amauryeb@gmail.com>
2022-04-25 22:08:30 -0300

    mudanca da logica

--- exemploGit/src/main/java/com/mycompany/exemplogit/exemplo1.java
+++ exemploGit/src/main/java/com/mycompany/exemplogit/exemplo1.java
@@ -5,7 +5,7 @@ package com.mycompany.exemplogit;
 public class exemplo1 {
 
     public static void main(String[] args) {
-        int  soma = 1 + 1 ;
+        int  soma = 1 + 98 ;
         System.out.println("Resultado da soma "+soma);
     }
     
