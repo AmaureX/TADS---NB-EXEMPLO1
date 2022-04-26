29ff50d436e51de7badde14c1f3cf8bdf7345f96
AmaureX <amauryeb@gmail.com>
2022-04-25 22:04:11 -0300

    Exemplo de commit Github

---
+++ exemploGit/src/main/java/com/mycompany/exemplogit/exemplo1.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.exemplogit;
+
+
+public class exemplo1 {
+
+    public static void main(String[] args) {
+        int  soma = 1 + 1 ;
+        System.out.println("Resultado da soma "+soma);
+    }
+    
+}
